6ca5e2e7beb82d688c57c0fb0608b39d631d7115
Wasim <yournew@email.com>
2026-05-25 09:27:00 +0200

    add user and admin roles and protect the routes

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,21 +1,43 @@
+import { auth, signOut } from "@/auth";
 import Link from "next/link";
 
-const Nav = () => {
+const Nav = async () => {
+  const session = await auth();
+  const userRole = session?.user?.role;
+  const isLoggedIn = !!session;
+
   return (
-    <nav className="shrink-0 h-20 w-full text-white bg-gray-800 p-4 flex items-center gap-6">
+    <nav className="shrink-0 h-20 w-full text-white bg-gray-800 p-4 flex items-center gap-6 justify-between ">
       {" "}
-      <Link href="/" className="hover:underline">
-        Dashboard
-      </Link>
-      <Link href="/products" className="hover:underline">
-        Products
-      </Link>
-      <Link href="/user" className="hover:underline">
-        User
-      </Link>
-      <Link href="/admin" className="hover:underline">
-        Admin
-      </Link>
+      <div className="flex gap-4">
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/dashboard" className="hover:underline">
+          Dashboard
+        </Link>
+        <Link href="/products" className="hover:underline">
+          Products
+        </Link>
+        {isLoggedIn && (
+          <Link href="/user" className="hover:underline">
+            User
+          </Link>
+        )}
+        {userRole === "ADMIN" && (
+          <Link href="/admin" className="hover:underline">
+            Admin
+          </Link>
+        )}
+      </div>
+      {session?.user && (
+        <>
+          <span className="font-bold text-right text-amber-200">
+            {" "}
+            Welcome, {session.user.name}
+          </span>
+        </>
+      )}
     </nav>
   );
 };
